6fc21ddcf214e6f6d5fe0890241a8be0dd091444
mikaelholtnielsen@gmail.com <mikaelholtnielsen@gmail.com>
2020-09-24 10:24:00 +0200

    Changed welcome text

--- PromotionEngine3/PromotionEngine3/Program.cs
+++ PromotionEngine3/PromotionEngine3/Program.cs
@@ -6,7 +6,7 @@ namespace PromotionEngine3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Promotion Engine");
         }
     }
 }
